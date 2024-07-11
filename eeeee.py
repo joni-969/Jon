0337c36f9eea88ddac484b8706f6959fab1a6e43
JonKadriu <jon.f.kadriu@gmail.com>
2024-07-11 11:47:47 +0200

    Add files via upload

---
+++ eeeee.py
@@ -0,0 +1,8 @@
+while True:
+    emri = input("Sheno username:")
+    if emri != "JonKadriu":
+        continue 
+    password = input("Shkruaje passwordin:")
+    if password == "jCoders":
+        break
+print("Emri dhe passwordi jane ne rregull")
